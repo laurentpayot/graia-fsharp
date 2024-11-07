16e8a260a4ccfbb0858d0519094a5b5cfa031a75
Laurent <laurent.payot@publiberty.com>
2024-11-07 17:35:55 +0100

    Tools.loadMnistCsv: int labels

--- src/Tools.fs
+++ src/Tools.fs
@@ -1,19 +1,23 @@
 ﻿module Tools
 
 open System.IO
+open System
 
 open FSharpPlus
 
 
-let loadMnistCsv (path: string) : string array * int seq array =
+let loadMnistCsv (path: string) : array<int> * array<seq<int>> =
     File.ReadAllText(path)
     |> String.split [ "\n" ]
+    // stop at the last empty line
+    |> Seq.takeWhile (not << String.IsNullOrWhiteSpace)
     // remove header row
     |> Seq.skip 1
     |> Seq.map (String.split [ "," ])
     |> fold
         (fun acc row ->
-            let label = Seq.head row
+
+            let label = Seq.head row |> int
 
             let data =
                 row
