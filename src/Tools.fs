d60fa7a0f9e5d8091eecbdda2b90f32a2681b45a
Laurent Payot <laurent.payot@publiberty.com>
2024-11-10 23:33:04 +0100

    Tools: showRowDigitBinarized + showRowDigit

--- src/Tools.fs
+++ src/Tools.fs
@@ -11,7 +11,9 @@ open Graia
 open Microsoft.DotNet.Interactive
 
 
-let loadMnistCsv (path: string) : array<int> * array<seq<byte>> =
+type ByteRow = seq<byte>
+
+let loadMnistCsv (path: string) : array<int> * array<ByteRow> =
     File.ReadAllText(path)
     |> String.split [ "\n" ]
     // stop at the last empty line
@@ -35,7 +37,7 @@ let loadMnistCsv (path: string) : array<int> * array<seq<byte>> =
     |> Array.ofSeq
     |> Array.unzip
 
-let byteRowsToBitArrays (threshold: byte) (byteRows: array<seq<byte>>) : array<BitArray> =
+let byteRowsToBitArrays (threshold: byte) (byteRows: array<ByteRow>) : array<BitArray> =
     byteRows
     |> Array.map ((Seq.map (fun v -> v >= threshold)) >> Array.ofSeq >> BitArray)
 
@@ -56,6 +58,25 @@ let weightsToMatrix (weights: Weights) : array<array<int>> =
                 | false, false -> 0)
             plusBools
             minusBools)
+
+let showRowDigitBinarized (threshold: byte) (row: ByteRow) : DisplayedValue =
+    let image =
+        row
+        |> Seq.map (fun x ->
+            if threshold = 0uy then int x
+            else if x >= threshold then 1
+            else 0)
+        |> Seq.chunkBySize 28
+        |> Seq.rev
+
+    let chart =
+        Chart.Heatmap(image, ColorScale = StyleParam.Colorscale.Greys, ShowScale = false)
+        |> Chart.withSize (100., 100.)
+        |> Chart.withMarginSize (0., 0., 0., 0.)
+
+    chart.Display()
+
+let showRowDigit (row: ByteRow) : DisplayedValue = showRowDigitBinarized 0uy row
 
 let showWeights (title: string) (weights: Weights) : DisplayedValue =
     let matrix = weights |> weightsToMatrix
